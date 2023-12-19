8bf6be61a9e001cf605259da2f9b82acb4206d1e
W. Meijer <houterm@gmail.com>
2023-12-19 09:26:44 +0100

    it works in two di kindoff

--- examples/diverge_from_point.py
+++ examples/diverge_from_point.py
@@ -1,5 +1,9 @@
+from typing import Callable
+
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
+from sympy import Matrix, diff, lambdify, sqrt, symbols
 
 from factor_graph.factor_graph import FactorGraph
 from factor_graph.gbp_settings import GBPSettings
@@ -13,8 +17,40 @@ from factor_graph.meas_model import MeasModel
 R_D = 10
 
 
+def get_jacobian() -> (
+    tuple[
+        Callable[[float, float, float, float, float], float],
+        Callable[[float, float, float, float, float], float],
+        Callable[[float, float, float, float, float], float],
+        Callable[[float, float, float, float, float], float],
+    ]
+):
+    # Define vector variables
+    x1, x2, y1, y2 = symbols("x1 x2 y1 y2")
+    z1, z2 = symbols("z1 z2")
+    CONSTANT = symbols("CONSTANT")
+
+    # Define the vectors as matrices
+    z1 = Matrix([x1, y1])
+    z2 = Matrix([x2, y2])
+
+    # Define the function
+    f = Matrix([1 - sqrt((z1 - z2).dot(z1 - z2)) / CONSTANT])
+
+    # Use the jacobian function to compute the Jacobian matrix
+    jacobian_matrix = f.jacobian(Matrix([[z1], [z2]]))
+    print(f"{jacobian_matrix=}")
+    j1_func = lambdify((x1, y1, x2, y2, CONSTANT), jacobian_matrix[0], "numpy")
+    j2_func = lambdify((x1, y1, x2, y2, CONSTANT), jacobian_matrix[1], "numpy")
+    j3_func = lambdify((x1, y1, x2, y2, CONSTANT), jacobian_matrix[2], "numpy")
+    j4_func = lambdify((x1, y1, x2, y2, CONSTANT), jacobian_matrix[3], "numpy")
+
+    return j1_func, j2_func, j3_func, j4_func
+
+
 def goal_meas_fn(x: torch.Tensor):
-    distance = x[0] - x[1]
+    distance = float(np.linalg.norm(x[0:2] - x[2:]))
+    print(f"{x=}")
     print(f"distance between goals: {distance}")
     if distance > R_D:
         return torch.tensor([[0.0]])
@@ -23,11 +59,19 @@ def goal_meas_fn(x: torch.Tensor):
 
 
 def goal_jac_fn(x: torch.Tensor):
-    distance = x[0] - x[1]
+    distance = float(np.linalg.norm(x[0:2] - x[2:]))
     if distance > R_D:
-        return torch.tensor([[0.0, 0.0]])
+        return torch.tensor([[0.0, 0.0, 0.0, 0.0]])
     else:
-        return torch.tensor([[-1 / R_D, 1 / R_D]])
+        j1_func, j2_func, j3_func, j4_func = get_jacobian()
+        x1, y1, x2, y2 = x[0], x[1], x[2], x[3]
+        j1 = float(j1_func(x1, y1, x2, y2, R_D))
+        j2 = float(j2_func(x1, y1, x2, y2, R_D))
+        j3 = float(j3_func(x1, y1, x2, y2, R_D))
+        j4 = float(j4_func(x1, y1, x2, y2, R_D))
+        print(f"{j1=}, {j2=}, {j3=}, {j4=}")
+
+        return torch.tensor([[j1, j2, j3, j4]])
 
 
 class DivergenceModel(MeasModel):
@@ -39,7 +83,7 @@ class DivergenceModel(MeasModel):
 
 
 """Set parameters"""
-n_varnodes = 4
+n_varnodes = 2
 x_range = 10
 
 gbp_settings = GBPSettings(
@@ -52,9 +96,14 @@ gbp_settings = GBPSettings(
 
 """Gaussian noise measurement model parameters:"""
 # set this one super high, cause we dont care about the prior
-prior_cov = torch.tensor([10.0])
+prior_cov = torch.tensor([10.0, 10.0])
+# prior_cov = torch.tensor([10.0, 10.0, 10.0, 10.0])
 
 # set this one super low, because we want to convergence to our measurements
+# smooth_cov = torch.tensor([0.0001, 0.0001, 0.0001, 0.0001])
+# smooth_cov = torch.tensor([0.0001, 0.0001, 0.0001, 0.0001])
+# smooth_cov = torch.tensor([0.0001, 0.0001, 0.0001])
+# smooth_cov = torch.tensor([0.0001, 0.0001])
 smooth_cov = torch.tensor([0.0001])
 
 
@@ -64,14 +113,18 @@ fg = FactorGraph(gbp_settings)
 xs = torch.linspace(0, x_range, n_varnodes).float().unsqueeze(0).T
 
 # initialize variable nodes. AKA With what resolution are we going to estimate the function?
-for i in range(n_varnodes):
-    prior_mean = torch.tensor([50.0])
-    fg.add_var_node(1, prior_mean, prior_cov)
+# for i in range(n_varnodes):
+prior_mean = torch.tensor([4.0, 51.0])
+fg.add_var_node(2, prior_mean, prior_cov)
+prior_mean = torch.tensor([5.0, 50.0])
+fg.add_var_node(2, prior_mean, prior_cov)
 
 # add smoothness factors between adjacent nodes
 for i in range(n_varnodes - 1):
     fg.add_factor(
-        [i, i + 1], torch.tensor([0.0]), DivergenceModel(SquaredLoss(1, smooth_cov))
+        [i, i + 1],
+        torch.tensor([0.1]),
+        DivergenceModel(SquaredLoss(1, smooth_cov)),
     )
 
 
@@ -81,7 +134,13 @@ fg.print(brief=True)
 
 # Beliefs are initialized to zero
 covs = torch.sqrt(torch.cat(fg.belief_covs()).flatten())
-plt.errorbar(xs, fg.belief_means(), yerr=covs, fmt="o", color="C0", label="Beliefs")
+print(f"{fg.belief_means()=} and {xs=}")
+# plt.errorbar(xs, fg.belief_means(), yerr=covs, fmt="o", color="C0", label="Beliefs")
+list_of_means = fg.belief_means()
+
+xss = list_of_means[::2]  # Select elements at even indices
+yss = list_of_means[1::2]  # Select elements at odd indices
+plt.errorbar(xss, yss, fmt="o", color="C0", label="Beliefs")
 plt.legend()
 plt.show()
 
@@ -90,7 +149,12 @@ plt.show()
 fg.gbp_solve(n_iters=50)
 
 # Plot beliefs and measurements
-covs = torch.sqrt(torch.cat(fg.belief_covs()).flatten())
-plt.errorbar(xs, fg.belief_means(), yerr=covs, fmt="o", color="C0", label="Beliefs")
+# covs = torch.sqrt(torch.cat(fg.belief_covs()).flatten())
+list_of_means = fg.belief_means()
+
+xss = list_of_means[::2]  # Select elements at even indices
+yss = list_of_means[1::2]  # Select elements at odd indices
+plt.errorbar(xss, yss, fmt="o", color="C0", label="Beliefs")
+# plt.errorbar(xs, fg.belief_means(), yerr=covs, fmt="o", color="C0", label="Beliefs")
 plt.legend()
 plt.show()
